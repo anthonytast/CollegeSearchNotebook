b37e95ecb1c51427a86c8af17006a3ab81a1df48
Anthony Tast <anthonyftast@gmail.com>
2022-09-11 22:42:30 -0400

    Initial Commit

---
+++ College Search Notebook/College_Search_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  College_Search_NotebookApp.swift
+//  College Search Notebook
+//
+//  Created by Anthony Tast on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct College_Search_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ College Search Notebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  College Search Notebook
+//
+//  Created by Anthony Tast on 9/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
